6e8489f8b84e94429e5d966670518e8f50f5079c
kumakoko <sun_of_lover@sina.com>
2022-06-11 18:00:05 +0800

    加入怪物类和pyganim模块

--- GameDef.py
+++ GameDef.py
@@ -15,3 +15,16 @@ MAP_COLUME_COUNT = 20
 MAP_ROW_COUNT = 15
 TILE_WIDTH = 40
 TILE_HEIGHT = 40
+
+#-------------------------恶魔的状态定义 开始-------------------------#
+GHOST_STATE_STRONG = 0      # 强壮的状态，这时候player可以被恶魔杀害
+GHOST_STATE_FLASHING = 1    # 恶魔闪烁的状态
+GHOST_STATE_WEAK = 2
+GHOST_STATE_DEAD = 3		# 恶魔死亡的状态
+#-------------------------恶魔的状态定义 结束-------------------------#
+
+#-------------------------恶魔的AI定义 开始-------------------------#
+GHOST_AI_AGGRESIVE = 0 # 追击player
+GHOST_AI_PASSIVE = 1   # 
+GHOST_AI_EVASIVE = 2   # 逃避player
+#-------------------------恶魔的AI定义 结束-------------------------#
--- Ghost.py
+++ Ghost.py
@@ -2,7 +2,7 @@ import pygame
 import GameDef
 
 
-class PacMan(pygame.sprite.Sprite):
+class Ghost(pygame.sprite.Sprite):
     FRAME_HEIGHT = 36
     FRAME_WIDTH = 36
 
@@ -10,14 +10,13 @@ class PacMan(pygame.sprite.Sprite):
         super().__init__()
         self.__sub_surface = []
         self.__current_frame = 0
-        self.__first_frame = 0  # 第一帧编号
-        self.__last_frame = 5  # 最后一帧编号
+        self.first_frame = 0  # 第一帧编号
+        self.last_frame = 5  # 最后一帧编号
         self.last_time = 0
         self.__center_pos = [60, 60]
-        self.__pos_rect = pygame.Rect(
-            0, 0, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
-        self.__pos_rect.left = self.__center_pos[0] - PacMan.FRAME_WIDTH/2
-        self.__pos_rect.top = self.__center_pos[1] - PacMan.FRAME_HEIGHT/2
+        self.__pos_rect = pygame.Rect(0, 0, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
+        self.__pos_rect.left = self.__center_pos[0] - Ghost.FRAME_WIDTH/2
+        self.__pos_rect.top = self.__center_pos[1] - Ghost.FRAME_HEIGHT/2
         self.__maze = None
 
     def Draw(self, surface):
@@ -33,16 +32,15 @@ class PacMan(pygame.sprite.Sprite):
 
     def __BuildSubSurface(self):
         for i in range(6):
-            src_rect = pygame.Rect(
-                0, i*PacMan.FRAME_HEIGHT, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
+            src_rect = pygame.Rect(0, i*Ghost.FRAME_HEIGHT, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
             self.__sub_surface.append(self.image.subsurface(src_rect))
 
     def update(self, current_time, rate=60):
         # 更新动画帧
         if current_time > self.last_time + rate:
             self.__current_frame += 1
-            if self.__current_frame > self.__last_frame:
-                self.__current_frame = self.__first_frame
+            if self.__current_frame > self.last_frame:
+                self.__current_frame = self.first_frame
             self.last_time = current_time
 
     def Move(self, event):
@@ -55,34 +53,34 @@ class PacMan(pygame.sprite.Sprite):
         if key_pressed[pygame.K_LEFT]:
             cx = cx - self.__move_speed
             new_rect = pygame.rect.Rect(
-                cx-PacMan.FRAME_WIDTH/2, cy-PacMan.FRAME_HEIGHT/2, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
+                cx-Ghost.FRAME_WIDTH/2, cy-Ghost.FRAME_HEIGHT/2, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
             is_block = self.__maze.IsBlock(new_rect, GameDef.DIR_LEFT)
             if not is_block:
                 self.__center_pos[0] = cx
         elif key_pressed[pygame.K_RIGHT]:
             cx = cx+self.__move_speed
             new_rect = pygame.rect.Rect(
-                cx-PacMan.FRAME_WIDTH/2, cy-PacMan.FRAME_HEIGHT/2, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
+                cx-Ghost.FRAME_WIDTH/2, cy-Ghost.FRAME_HEIGHT/2, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
             is_block = self.__maze.IsBlock(new_rect, GameDef.DIR_RIGHT)
             if not is_block:
                 self.__center_pos[0] = cx
         elif key_pressed[pygame.K_UP]:
             cy = cy-self.__move_speed
             new_rect = pygame.rect.Rect(
-                cx-PacMan.FRAME_WIDTH/2, cy-PacMan.FRAME_HEIGHT/2, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
+                cx-Ghost.FRAME_WIDTH/2, cy-Ghost.FRAME_HEIGHT/2, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
             is_block = self.__maze.IsBlock(new_rect, GameDef.DIR_UP)
             if not is_block:
                 self.__center_pos[1] = cy
         elif key_pressed[pygame.K_DOWN]:
             cy = cy+self.__move_speed
             new_rect = pygame.rect.Rect(
-                cx-PacMan.FRAME_WIDTH/2, cy-PacMan.FRAME_HEIGHT/2, PacMan.FRAME_WIDTH, PacMan.FRAME_HEIGHT)
+                cx-Ghost.FRAME_WIDTH/2, cy-Ghost.FRAME_HEIGHT/2, Ghost.FRAME_WIDTH, Ghost.FRAME_HEIGHT)
             is_block = self.__maze.IsBlock(new_rect, GameDef.DIR_DOWN)
             if not is_block:
                 self.__center_pos[1] = cy
 
-        self.__pos_rect.left = self.__center_pos[0] - PacMan.FRAME_WIDTH/2
-        self.__pos_rect.top = self.__center_pos[1] - PacMan.FRAME_HEIGHT/2
+        self.__pos_rect.left = self.__center_pos[0] - Ghost.FRAME_WIDTH/2
+        self.__pos_rect.top = self.__center_pos[1] - Ghost.FRAME_HEIGHT/2
 
     def GetCenterPos(self):
         return self.__center_pos
